199f3a277a3fc3f03f43fea387419dfa4e989fe3
MelchMwoan <Paarlbergamy@gmail.com>
2023-09-29 11:17:03 +0200

    Removed Unnecessary pages

--- GameNight2/Controllers/HomeController.cs
+++ GameNight2/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GameNight2.Controllers
 			return View();
 		}
 
+		public IActionResult Account()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
