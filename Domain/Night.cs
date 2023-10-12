5f650d9b17782202d70da3d3868f3e876ca88796
MelchMwoan <Paarlbergamy@gmail.com>
2023-10-12 18:35:01 +0200

    Added check for adult on join night

--- Domain/Night.cs
+++ Domain/Night.cs
@@ -16,12 +16,14 @@ namespace Domain
 		public Person Organisator { get; set; }
 		public List<Review> Reviews { get; set; }
 		public List<Snack> Snacks { get; set; }
+		public bool AdultOnly { get; set; } = false;
 		public string ThumbnailUrl { get; set; } = "https://s.yimg.com/ny/api/res/1.2/GjwW45jyFilXfDhM3Pl5rQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02NzU-/https://media.zenfs.com/en/gobankingrates_644/4464a542bd2bf3c1300d1ae8de26441b";
 
 		public void AddPlayer(Person player)
 		{
 			if (Organisator.Email.Equals(player.Email)) throw new Exception("Can't join your own night");
 			if(Players.Count >= MaxPlayers) throw new Exception("The Night is already full");
+			if(AdultOnly && (DateTime.Now.Year - player.BirthDate.Year - (player.BirthDate.DayOfYear > DateTime.Now.DayOfYear ? 1 : 0)) < 18) throw new Exception("The Night is for adults only");
 			Players.Add(player);
 		}
 
