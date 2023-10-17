26fc3d2a9b4a8772f3e802854fdd82898ba46144
MelchMwoan <Paarlbergamy@gmail.com>
2023-10-17 14:12:58 +0200

    Create/Get snack functionality

---
+++ GameNight2/Controllers/SnackController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Domain;
+using DomainServices;
+using GameNight2.Models;
+using Infrastructure.EF;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SQLData;
+
+namespace GameNight2.Controllers
+{
+	public class SnackController : Controller
+	{
+		private readonly ILogger<SnackController> _logger;
+		private ISnackRepository _snackRepository;
+		private UserManager<GameNight2User> _userManager;
+		private IAccountRepository _accountRepository;
+
+		public SnackController(ILogger<SnackController> logger, ISnackRepository snackRepository, UserManager<GameNight2User> userManager, IAccountRepository accountRepository)
+		{
+			_snackRepository = snackRepository;
+			_logger = logger;
+			_userManager = userManager;
+			_accountRepository = accountRepository;
+		}
+
+		public List<Snack> GetSnacks()
+		{
+			return _snackRepository.getSnacks();
+		}
+
+		[HttpGet]
+		public IActionResult CreateSnack()
+		{
+			return View();
+		}
+
+		//[HttpPost]
+		//public IActionResult CreateGame(NewGameModel gameModel)
+		//{
+		//	if (!ModelState.IsValid)
+		//	{
+		//		return BadRequest(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+		//	};
+		//	Game game = gameModel.getGame();
+		//	_gameRepository.addGame(game);
+		//	return Ok();
+		//}
+
+		public IActionResult SnackDetails(int Id)
+		{
+			Snack snack = _snackRepository.getSnackById(Id);
+			if(snack != null) return View(snack);
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
---
+++ GameNight2/Models/NewSnackModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain;
+using DomainServices;
+
+namespace GameNight2.Models
+{
+	public class NewSnackModel
+	{
+		public string Name { get; set; }
+		public Boolean isVegan { get; set; }
+		public Boolean isAlcoholFree { get; set; }
+		public Boolean isVegatarian { get; set; }
+		public Boolean isGlutenFree { get; set; }
+		public Boolean isLactoseFree { get; set; }
+		public Boolean isNutsFree { get; set; }
+	}
+}
