2ea238aaed4a72927966d5d3b811f248c7d1f0e7
MelchMwoan <Paarlbergamy@gmail.com>
2023-10-23 10:42:02 +0200

    Added Review repository

---
+++ DomainServices/IReviewRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Infrastructure.EF;
+
+namespace DomainServices
+{
+	public interface IReviewRepository
+	{
+		List<Review> GetReviews();
+		Review? GetReviewById(int id);
+		List<Review> GetReviewByNight(int nightId);
+		List<Review> GetReviewByOrganisator(int organisatorId);
+		void AddReview(Review review);
+		void RemoveReview(Review review);
+	}
+}
---
+++ Infrastructure.EF/ReviewEFRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DomainServices;
+using Microsoft.EntityFrameworkCore;
+using SQLData;
+
+namespace Infrastructure.EF
+{
+	public class ReviewEFRepository : IReviewRepository
+	{
+		private GameNightDbContext _dbContext;
+
+		public ReviewEFRepository(GameNightDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public List<Review> GetReviews()
+		{
+			return _dbContext.Reviews.ToList();
+		}
+
+		public Review? GetReviewById(int id)
+		{
+			return _dbContext.Reviews.FirstOrDefault(r => r.Id == id);
+		}
+
+		public List<Review> GetReviewByNight(int nightId)
+		{
+			return _dbContext.Reviews.Where(r => r.nightId == nightId).ToList();
+		}
+
+		public List<Review> GetReviewByOrganisator(int organisatorId)
+		{
+			return _dbContext.Reviews.Where(r => r.night.Organisator.Id == organisatorId).ToList();
+		}
+
+		public void AddReview(Review review)
+		{
+			_dbContext.Reviews.Add(review);
+			_dbContext.SaveChanges();
+		}
+
+		public void RemoveReview(Review review)
+		{
+			_dbContext.Reviews.Remove(review);
+			_dbContext.SaveChanges();
+		}
+	}
+}
