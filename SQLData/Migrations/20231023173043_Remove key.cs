0496634c68c0a6e014068a2853253503ee9d44a2
MelchMwoan <Paarlbergamy@gmail.com>
2023-10-23 19:33:45 +0200

    Updated DB key

---
+++ SQLData/Migrations/20231023173043_Remove key.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SQLData.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removekey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Persons_organisatorId",
+                table: "Reviews");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Persons_organisatorId",
+                table: "Reviews",
+                column: "organisatorId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Persons_organisatorId",
+                table: "Reviews");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Persons_organisatorId",
+                table: "Reviews",
+                column: "organisatorId",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+    }
+}
